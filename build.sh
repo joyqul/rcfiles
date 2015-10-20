8d18ddc2d985da293c344d3c79739117061da0e6
joyqul <joyqul.tw@gmail.com>
2015-10-20 14:20:35 +0800

    add zencode

---
+++ build.sh
@@ -0,0 +1,7 @@
+cd ~/.vim
+wget http://www.vim.org/scripts/download_script.php?src_id=23483
+unzip emmet-vim.zip
+mkdir ~/.vim/bundle
+cd ~/.vim/bundle
+git clone https://github.com/mattn/emmet-vim.git
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
