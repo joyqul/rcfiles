971c563d8dbb829d1eb6fbf385e83d3c588cf7ab
joyqul Chen <joy1357924680@gmail.com>
2015-10-20 14:58:13 +0800

    bug fix

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+mkdir ~/.vim
 cd ~/.vim
 wget -O emmet-vim.zip http://www.vim.org/scripts/download_script.php?src_id=23483
 unzip emmet-vim.zip
@@ -5,3 +6,4 @@ mkdir ~/.vim/bundle
 cd ~/.vim/bundle
 git clone https://github.com/mattn/emmet-vim.git
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp ~/rcfiles/.vimrc ~/
